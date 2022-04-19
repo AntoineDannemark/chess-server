83d9ff0992b7ea90fe5b1333fbc9ba756391ef21
Antoine <antoine.dannemark@protonmail.com>
2022-04-19 21:39:29 +0200

    start basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("test")
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => res.send("HELLO YOU"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
